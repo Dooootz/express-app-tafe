1cb2ff7560cd1fad636aeee90a70c92277655cdb
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-29 23:54:51 +1000

    improved readability

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,5 @@
-// import mongoose
 import mongoose from 'mongoose';
-// import our created User schema
 import { UserSchema } from "../models/userModel.js";
-// import bcrypt to hash all user passwords 
 import bcrypt from 'bcrypt';
 
 // makes a copy of the schema before executing queries to the DB 
@@ -10,24 +7,17 @@ const User = mongoose.model('User', UserSchema);
 
 // add new user controller 
 export const addNewUser = (req, res) => {
-    // instantiate new user - takes the request body as an arguement or parameter
     const newUser = new User(req.body);
-    // uses bcrypt to hash user password -:- hashSync(data, salt, optional: callback)
     newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
 
-    // .save function saves document by inserting new document to the DB 
-    // takes 2 parameters - err & user 
     newUser.save((err, user) => {
-        // if err... then return response status 400 & send error message
         if (err) {
             return res.status(400).send({
                 message: err
             });
         
         } else {
-            // hide hashed password from end user
             user.hashPassword = undefined;
-            // return res status 201 & json data of user
             return res.status(201).json(user);
         }
     })
@@ -35,14 +25,11 @@ export const addNewUser = (req, res) => {
 
 // update user controller
 export const updateUser = (req, res) => {
-    // findOneAndUpdate(conditions, update, options, callback)  - returns query
     User.findOneAndUpdate({_id: req.params.userID}, req.body, {
         new: true, useFindAndModify: false
     },(err, user) => {
-        // if error send err
         if (err) {
             res.send(err);
-            // else return response as json
         } else {
             res.json(user);
         }
@@ -51,16 +38,11 @@ export const updateUser = (req, res) => {
 
 // GET user controller
 export const getUsers = (req, res) => {
-    // mongoose casts the filter to match the models schema
-    // before the command is sent 
-    //.find(id, callback)
     User.find({}, (err, user) => {
         if (err) {
             res.send(err);
         } else {
-            // hide hashed password from end user
             user.hashPassword = undefined;
-            // return user data as json
             res.json(user);
         }
     })
@@ -68,7 +50,6 @@ export const getUsers = (req, res) => {
 
 // GET user by id controller
 export const getUserById = (req, res) => {
-    // .findById(id, callback) parameters
     User.findById(req.params.userID, (err, user) => {
         if (err) {
             res.send(err);
@@ -80,8 +61,6 @@ export const getUserById = (req, res) => {
 
 // DELETE user controller
 export const deleteUser = (req, res) => {
-    // .remove(id, callback) parameters
-    // removes all documents the match the user id 
     User.remove({_id: req.params.userID}, (err, user) => {
         if (err) {
             res.send(err);
@@ -92,3 +71,101 @@ export const deleteUser = (req, res) => {
         }
     })
 }
+
+
+
+
+// // import mongoose
+// import mongoose from 'mongoose';
+// // import our created User schema
+// import { UserSchema } from "../models/userModel.js";
+// // import bcrypt to hash all user passwords 
+// import bcrypt from 'bcrypt';
+
+// // makes a copy of the schema before executing queries to the DB 
+// const User = mongoose.model('User', UserSchema);
+
+// // add new user controller 
+// export const addNewUser = (req, res) => {
+//     // instantiate new user - takes the request body as an arguement or parameter
+//     const newUser = new User(req.body);
+//     // uses bcrypt to hash user password -:- hashSync(data, salt, optional: callback)
+//     newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+
+//     // .save function saves document by inserting new document to the DB 
+//     // takes 2 parameters - err & user 
+//     newUser.save((err, user) => {
+//         // if err... then return response status 400 & send error message
+//         if (err) {
+//             return res.status(400).send({
+//                 message: err
+//             });
+        
+//         } else {
+//             // hide hashed password from end user
+//             user.hashPassword = undefined;
+//             // return res status 201 & json data of user
+//             return res.status(201).json(user);
+//         }
+//     })
+// }
+
+// // update user controller
+// export const updateUser = (req, res) => {
+//     // findOneAndUpdate(conditions, update, options, callback)  - returns query
+//     User.findOneAndUpdate({_id: req.params.userID}, req.body, {
+//         new: true, useFindAndModify: false
+//     },(err, user) => {
+//         // if error send err
+//         if (err) {
+//             res.send(err);
+//             // else return response as json
+//         } else {
+//             res.json(user);
+//         }
+//     })
+// }
+
+// // GET user controller
+// export const getUsers = (req, res) => {
+//     // mongoose casts the filter to match the models schema
+//     // before the command is sent 
+//     //.find(id, callback)
+//     User.find({}, (err, user) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             // hide hashed password from end user
+//             user.hashPassword = undefined;
+//             // return user data as json
+//             res.json(user);
+//         }
+//     })
+// }
+
+// // GET user by id controller
+// export const getUserById = (req, res) => {
+//     // .findById(id, callback) parameters
+//     User.findById(req.params.userID, (err, user) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.json(user);
+//         }
+//     })
+// }
+
+// // DELETE user controller
+// export const deleteUser = (req, res) => {
+//     // .remove(id, callback) parameters
+//     // removes all documents the match the user id 
+//     User.remove({_id: req.params.userID}, (err, user) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.json({
+//                 message: "Deleted Successfully!"
+//             });
+//         }
+//     })
+// }
